54d6d0abba90a8130b9ee270afb7a5e787b6cfac
Marco Braak <mbraak@ridethepony.nl>
2014-06-27 09:26:27 +0200

    Added auth and session middleware in settings for example project

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from django.conf import global_settings
+
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 sys.path.append(
@@ -37,6 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admin',
 ]
+
+MIDDLEWARE_CLASSES = list(global_settings.MIDDLEWARE_CLASSES)
+
+session_middleware = 'django.contrib.sessions.middleware.SessionMiddleware'
+
+if session_middleware not in MIDDLEWARE_CLASSES:
+    MIDDLEWARE_CLASSES.append(session_middleware)
+
+authentication_middleware = 'django.contrib.auth.middleware.AuthenticationMiddleware'
+
+if authentication_middleware not in MIDDLEWARE_CLASSES:
+    MIDDLEWARE_CLASSES.append(authentication_middleware)
 
 STATIC_URL = '/static/'
 ROOT_URLCONF = 'example_project.urls'
