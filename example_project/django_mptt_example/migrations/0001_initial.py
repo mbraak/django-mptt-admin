406b179f158978458de19516b534dbace481fa5f
Marco Braak <mbraak@ridethepony.nl>
2015-04-02 12:32:16 +0200

    Added migrations for example project

---
+++ example_project/django_mptt_example/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import mptt.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Country',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('code', models.CharField(max_length=2, null=True, blank=True)),
+                ('name', models.CharField(max_length=50, null=True, blank=True)),
+                ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('level', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name='children', blank=True, to='django_mptt_example.Country', null=True)),
+            ],
+            options={
+                'verbose_name_plural': 'countries',
+            },
+        ),
+    ]
