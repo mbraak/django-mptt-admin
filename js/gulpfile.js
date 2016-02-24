b3c3c490f29bef809c11780dea8141a6bfcb02f5
Marco Braak <mbraak@ridethepony.nl>
2016-02-24 15:01:50 +0100

    Use sass

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
+const sass = require("gulp-sass");
 
 
-gulp.task("default", () =>
+gulp.task("js", () =>
     gulp.src("*.js")
         .pipe(babel())
         .pipe(gulp.dest("../django_mptt_admin/static/django_mptt_admin/"))
 );
 
-gulp.task("watch", ["default"], () =>
-    gulp.watch(["./*.js"], ["default"])
+gulp.task("sass", () =>
+    gulp.src("*.scss")
+        .pipe(sass({ errLogToConsole: true }))
+        .pipe(gulp.dest("../django_mptt_admin/static/django_mptt_admin/"))
 );
+
+gulp.task("default", ["js", "sass"]);
+
+gulp.task("watch", ["js", "sass"], () => {
+    gulp.watch(["./*.js"], ["js"]);
+    gulp.watch(["./*.scss"], ["sass"]);
+});
