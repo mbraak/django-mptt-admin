36f290add4caad4d8910488baa6055d089f0c646
Marco Braak <marco@invoicefinance.nl>
2020-06-10 08:42:46 +0200

    Lint

--- frontend/django_mptt_admin.ts
+++ frontend/django_mptt_admin.ts
@@ -4,6 +4,22 @@ import * as cookie from "cookie";
 import * as URL from "url-parse";
 import * as qs from "querystringify";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+interface JQTreeMoveEvent extends JQuery.Event {
+    move_info: {
+        moved_node: INode;
+        position: string;
+        target_node: INode;
+        do_move: () => void;
+    };
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+interface JQTreeSelectEvent extends JQuery.Event {
+    deselected_node: INode | null;
+    node: INode;
+}
+
 function initTree(
     $tree: JQuery,
     autoOpen: boolean | number,
@@ -11,7 +27,7 @@ function initTree(
     rtl: boolean,
     csrfCookieName: string
 ) {
-    let errorNode: any = null;
+    let errorNode: INode | null = null;
     const insertAtUrl = new URL($tree.data("insert_at_url"), true);
 
     function createLi(node: INode, $li: JQuery, isSelected: boolean) {
@@ -26,7 +42,7 @@ function initTree(
 
         $title.after(
             `<a href="${
-                node.url
+                node.url as string
             }" class="edit" tabindex="${tabindex}">(${gettext("edit")})</a>`,
             `<a href="${insertUrlString}" class="edit" tabindex="${tabindex}">(${gettext(
                 "add"
@@ -52,13 +68,14 @@ function initTree(
         return getFromCookie() || getFromMiddleware();
     }
 
-    function handleMove(e: any) {
+    function handleMove(eventParam: JQuery.Event) {
+        const e = eventParam as JQTreeMoveEvent;
         const info = e.move_info;
         const data = {
             target_id: info.target_node.id,
-            position: info.position
+            position: info.position,
         };
-        const $el = jQuery(info.moved_node.element);
+        const $el = jQuery(info.moved_node.element) as JQuery<HTMLElement>;
 
         handleLoading(true, null, $el);
 
@@ -66,11 +83,11 @@ function initTree(
 
         e.preventDefault();
 
-        jQuery.ajax({
+        void jQuery.ajax({
             type: "POST",
-            url: info.moved_node.move_url,
+            url: info.moved_node.move_url as string,
             data,
-            beforeSend: xhr => {
+            beforeSend: (xhr) => {
                 // Set Django csrf token
                 xhr.setRequestHeader("X-CSRFToken", getCsrfToken());
             },
@@ -88,14 +105,12 @@ function initTree(
                 );
 
                 errorNode = info.moved_node;
-            }
+            },
         });
 
         function removeErrorMessage() {
             if (errorNode) {
-                jQuery(errorNode.element)
-                    .find(".mptt-admin-error")
-                    .remove();
+                jQuery(errorNode.element).find(".mptt-admin-error").remove();
                 errorNode = null;
             }
         }
@@ -105,7 +120,7 @@ function initTree(
         $tree.html(gettext("Error while loading the data from the server"));
     }
 
-    const spinners: any = {};
+    const spinners: Record<number | string, Spinner | null> = {};
 
     function handleLoading(
         isLoading: boolean,
@@ -142,21 +157,18 @@ function initTree(
         }
     }
 
-    function handleSelect(e: any) {
+    function handleSelect(eventParam: JQuery.Event) {
+        const e = eventParam as JQTreeSelectEvent;
         const { node, deselected_node } = e;
 
         if (deselected_node) {
             // deselected node: remove tabindex
-            jQuery(deselected_node.element)
-                .find(".edit")
-                .attr("tabindex", -1);
+            jQuery(deselected_node.element).find(".edit").attr("tabindex", -1);
         }
 
         if (node) {
             // selected: add tabindex
-            jQuery(node.element)
-                .find(".edit")
-                .attr("tabindex", 0);
+            jQuery(node.element).find(".edit").attr("tabindex", 0);
         }
     }
 
@@ -169,15 +181,15 @@ function initTree(
         onCreateLi: createLi,
         onLoadFailed: handleLoadFailed,
         onLoading: handleLoading,
-        saveState: $tree.data("save_state"),
-        useContextMenu: Boolean($tree.data("use_context_menu"))
+        saveState: $tree.data("save_state") as boolean,
+        useContextMenu: Boolean($tree.data("use_context_menu")),
     });
 
     $tree.on("tree.move", handleMove);
     $tree.on("tree.select", handleSelect);
 }
 
-function urlToString(url: any) {
+function urlToString(url: URL): string {
     const { pathname, query } = url;
     const querystring = qs.stringify(query);
 
@@ -192,10 +204,10 @@ jQuery(() => {
     const $tree = jQuery("#tree");
 
     if ($tree.length) {
-        const autoOpen = $tree.data("auto_open");
+        const autoOpen = $tree.data("auto_open") as boolean | number;
         const autoEscape = Boolean($tree.data("autoescape"));
         const rtl = $tree.data("rtl") === "1";
-        const csrfCookieName = $tree.data("csrf-cookie-name");
+        const csrfCookieName = $tree.data("csrf-cookie-name") as string;
 
         initTree($tree, autoOpen, autoEscape, rtl, csrfCookieName);
     }
