9d5335fa036ab94227d3f7da8fea42cfbf416264
Marco Braak <marco@invoicefinance.nl>
2020-06-26 11:20:56 +0200

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '1.0.1'
+version = '1.0.2'
 
 setup(
     name='django-mptt-admin',
