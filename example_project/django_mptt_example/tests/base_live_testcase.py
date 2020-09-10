00c22dd555aa14a62bcd7716bf1b8b5ddfb95210
Marco Braak <marco@invoicefinance.nl>
2020-09-10 14:55:41 +0200

    Generate js coverage

--- example_project/django_mptt_example/tests/base_live_testcase.py
+++ example_project/django_mptt_example/tests/base_live_testcase.py
@@ -1,17 +1,9 @@
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import django
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.firefox.options import Options
 
 
 class BaseLiveTestCase(StaticLiveServerTestCase):
-    def _post_teardown(self):
-        if django.VERSION < (2, 2):
-            ContentType.objects.clear_cache()
-
-        super()._post_teardown()
-
     def setUp(self):
         super().setUp()
 
