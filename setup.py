284c8b547b09175254ab702bc197eac66acd918a
Marco Braak <marco.braak@planet.nl>
2021-12-08 12:34:12 +0100

    Django 4 (#360)
    * Test with Django 4.0 development version
    
    * Add Django 4.0 to setup.py
    
    * Run tests with verbose flag
    
    * Add migration
    
    * Set search_help_text parameter in Django 4
    
    * Set USE_TZ setting in example project
    
    * Test with Django 4.0 alpha 1
    
    * Test with Django 4.0 beta 1
    
    * Pin psycopg2 requirement
    
    * Test with Django 4.0rc1
    
    * Support Python 3.10
    
    * Update readme

--- setup.py
+++ setup.py
@@ -29,12 +29,14 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython"
     ]
 )
