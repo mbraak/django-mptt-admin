2e15d9210093cd841b95aad7e6e342649ad1581a
Marco Braak <marco@floryn.com>
2023-12-06 15:55:57 +0100

    Remove Django 4.0 from setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
         "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.0",
         "Intended Audience :: Developers",
