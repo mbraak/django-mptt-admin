65c6c31ce63576f2530ea265174aef183311041d
Marco Braak <marco@floryn.com>
2024-04-08 16:23:39 +0200

    Flat config

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,15 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    }
+);
