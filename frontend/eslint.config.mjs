d428a68d4973cfa65d6ad332827baac67f8774c7
Marco Braak <marco@floryn.com>
2024-09-22 08:59:18 +0200

    Eslint 9; drop eslint-import and deprecation (#577)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,14 +1,10 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import importPlugin from "eslint-plugin-import";
-import eslintPluginDeprecation from "eslint-plugin-deprecation";
 
 export default [
     eslint.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
     ...tseslint.configs.stylisticTypeChecked,
-    importPlugin.flatConfigs.recommended,
-    importPlugin.flatConfigs.typescript,
     {
         languageOptions: {
             parserOptions: {
@@ -16,12 +12,8 @@ export default [
                 tsconfigRootDir: import.meta.dirname,
             },
         },
-        plugins: {
-            deprecation: eslintPluginDeprecation,
-        },
         rules: {
             "@typescript-eslint/restrict-template-expressions": "error",
-            "deprecation/deprecation": "error",
         },
     },
 ];
