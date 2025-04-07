443ace8dc283bf0310308a39aba0029519f78850
Marco Braak <marco@floryn.com>
2025-04-07 21:37:02 +0200

    Version 2.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '2.7.2'
+version = '2.8.0'
 
 setup(
     name='django-mptt-admin',
