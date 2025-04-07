0a76d97f344e02b7188fb3396deb7158bb98e91e
Marco Braak <marco@floryn.com>
2025-04-07 21:28:02 +0200

    Support Django 5.2; drop Django 5.0 (#636)

--- setup.py
+++ setup.py
@@ -27,15 +27,14 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
         "Framework :: Django :: 4.2",
-        "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
+        "Framework :: Django :: 5.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: Implementation :: CPython"
     ]
 )
