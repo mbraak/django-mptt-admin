fb93326770147cd8843138954820c0cabf88e5b4
Marco Braak <marco@floryn.com>
2025-04-15 21:48:37 +0200

    Load tree in js test

--- frontend/src/initTree.test.ts
+++ frontend/src/initTree.test.ts
@@ -1,11 +1,43 @@
 import { screen } from "@testing-library/dom";
 import jQuery from "jquery";
-import { expect, test } from "vitest";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import { afterAll, afterEach, beforeAll, expect, test } from "vitest";
 
 import initTree from "./initTree";
 
-test("initializes the tree", () => {
+const server = setupServer();
+const treeData = [
+    {
+        children: [
+            {
+                id: 2,
+                name: "Africa",
+            },
+        ],
+        id: 1,
+        name: "root",
+    },
+];
+server.use(http.get("/tree", () => new HttpResponse(JSON.stringify(treeData))));
+
+beforeAll(() => {
+    server.listen();
+});
+
+afterEach(() => {
+    server.resetHandlers();
+});
+
+afterAll(() => {
+    server.close();
+});
+
+test("initializes the tree", async () => {
+    window.gettext = (key) => key;
+
     const treeElement = document.createElement("div");
+    treeElement.setAttribute("data-url", "/tree");
     document.body.append(treeElement);
 
     const $tree = jQuery(treeElement);
@@ -21,5 +53,8 @@ test("initializes the tree", () => {
         rtl: false,
     });
 
-    expect(screen.getByRole("tree")).toBeInTheDocument();
+    expect(await screen.findByRole("tree")).toBeInTheDocument();
+    expect(
+        await screen.findByRole("treeitem", { name: "Africa" })
+    ).toBeInTheDocument();
 });
